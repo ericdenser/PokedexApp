f39cf994be023feb3d6b474095dc8b9df4d600bc
ericdenser <ericdenser7@gmail.com>
2025-07-29 14:09:17 -0300

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Eric on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexAppTests/PokedexAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppTests.swift
+//  PokedexAppTests
+//
+//  Created by Eric on 29/07/25.
+//
+
+import Testing
+@testable import PokedexApp
+
+struct PokedexAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
